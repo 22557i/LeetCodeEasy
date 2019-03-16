627166db9e6651c74196061c14a12e9c67d82a96
Hao_Wu_001443861 <wu.hao8@husky.neu.edu>
2019-03-16 13:12:24 -0400

    2019.3.17
    LCeasy13

--- LCeasy13/src/lcwu/Solution13.java
+++ LCeasy13/src/lcwu/Solution13.java
@@ -3,22 +3,29 @@ package lcwu;
 public class Solution13 {
     public int romanToInt(String s){
        int total=0;
-       for(int i =0;i<s.length();i++)
+       for(int i =0;i<s.length();i++) {
            switch (s.charAt(i)) {
-               case 'I':
-                   if (s.charAt(i + 1) == 'V') {
+               case 'I':{
+                   if(i!=s.length()-1) {
+            	   if (s.charAt(i + 1) == 'V') {
                        total += 4;
                        i++;
+                       break;
                    } else if (s.charAt(i + 1) == 'X') {
                        total += 9;
                        i++;
+                       break;
                    } else
                        total += 1;
-
+                       break;
+               }else total+=1; break;
+                   }
                case 'V':
                    total += 5;
+                   break;
                case 'X':
-                   if (s.charAt(i + 1) == 'L') {
+                   if(i!=s.length()-1) {
+            	   if (s.charAt(i + 1) == 'L') {
                        total += 40;
                        i++;
                    } else if (s.charAt(i + 1) == 'C') {
@@ -26,9 +33,13 @@ public class Solution13 {
                        i++;
                    } else
                        total += 10;
+                       break;
+                   }else total+=10;break;
                case 'L':
                    total += 50;
+                   break;
                case 'C':
+            	   if(i!=s.length()-1) {
                    if (s.charAt(i + 1) == 'D') {
                        total += 400;
                        i++;
@@ -37,16 +48,22 @@ public class Solution13 {
                        i++;
                    } else
                        total += 100;
+                       break;}
+            	   else total+=100;break;
                case 'D':
                    total += 500;
+                   break;
+                   
                case 'M':
                    total += 1000;
+                   break;
            }
+       }
        return total;
     }
     public static void main(String args[]){
         Solution13 s13 = new Solution13();
-        String a = "LVIII";
+        String a = "MCMXCIV";
         System.out.println(s13.romanToInt(a));
     }
 }
