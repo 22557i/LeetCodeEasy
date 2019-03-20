d9c1d9911233a42ae3ba71e8f136c899fc5b5b9a
HAO_WU_001443861 <wu.hao8@husky.neu.edu>
2019-03-20 19:55:51 -0400

    3.20

---
+++ LCeasy107/src/lcwu/Solution107.java
@@ -0,0 +1,29 @@
+package lcwu;
+
+import java.util.*;
+
+public class Solution107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if(root==null)
+            return new LinkedList();
+        Queue<TreeNode> queue=new LinkedList<TreeNode>();
+        queue.add(root);
+        LinkedList<List<Integer>> res=new LinkedList<List<Integer>>();
+        while(!queue.isEmpty())
+        {
+            int n=queue.size();
+            LinkedList<Integer> l=new LinkedList<>();
+            for(int i=0;i<n;i++)
+            {
+                TreeNode temp=queue.poll();
+                l.add(temp.val);
+                if(temp.left!=null)
+                    queue.add(temp.left);
+                if(temp.right!=null)
+                    queue.add(temp.right);
+            }
+            res.addFirst(new LinkedList(l));
+        }
+        return res;
+    }
+}
