fa8571f026e5e61c12764f0d659c78d840e8d593
HAO_WU_001443861 <wu.hao8@husky.neu.edu>
2019-03-19 20:35:57 -0400

    3.19
    3.19easy100

--- LCeasy100/src/lcwu/Solution100.java
+++ LCeasy100/src/lcwu/Solution100.java
@@ -1,10 +1,29 @@
 package lcwu;
 
 public class Solution100 {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null&&q==null)return true;
-        if(p==null||q==null)return false;
-        if(p.val!=q.val)return false;
-        return isSameTree(p.left,q.left)&& isSameTree(q.right,q.right);
+
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if(p==null&&q==null){
+                return true;
+            }
+            if(p==null||q==null){
+                return false;
+            }
+            if(p!=null&&q!=null&&p.val!=q.val){
+                return false;
+            }
+
+            boolean falg1 = isSameTree(p.left,q.left);
+
+
+            boolean falg2 = isSameTree(p.right,q.right);
+
+
+            if(falg1==false||falg2==false){
+                return false;
+            }
+
+            return true;
+        }
     }
-}
+
--- LCeasy88/src/lcwu/Solution88.java
+++ LCeasy88/src/lcwu/Solution88.java
@@ -2,28 +2,37 @@ package lcwu;
 
 public class Solution88 {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-       if(n!=0) {
-           for (int i = m; i < m + n; i++) {
-               nums1[m] = nums2[i - m];
-           }
+
+           int sum[]=new int[m+n];
+       int i =0;
+       for(;i<m;i++){
+           sum[i]=nums1[i];
        }
+       for(i =0;i<n;i++){
+           sum[i+m]=nums2[i];
+       }
+
+
         int temp=0;
         for(int j=0;j<m+n-1;j++){
-            for(int k=j;k<m+n-1;k++){
-                if(nums1[k]>nums1[k+1]){
-                    temp = nums1[k];
-                    nums1[k]=nums1[k+1];
-                    nums1[k+1]=temp;
+            for(int k=0;k<m+n-1;k++){
+                if(sum[k]>sum[k+1]){
+                    temp = sum[k];
+                    sum[k]=sum[k+1];
+                    sum[k+1]=temp;
                 }
 
             }
         }
+        for(i=0;i<m+n;i++){
+            nums1[i]=sum[i];
+        }
     }
     public static void main(String args[]){
-        int nums[]={1,2,4,5,6,0};
-        int ai[]={3};
+        int nums[]={4,5,6,0,0,0};
+        int ai[]={1,2,3};
         Solution88 s88 = new Solution88();
-        s88.merge(nums,5,ai,1);
+        s88.merge(nums,3,ai,3);
         for(int i =0;i<6;i++){
             System.out.println(nums[i]);
         }
