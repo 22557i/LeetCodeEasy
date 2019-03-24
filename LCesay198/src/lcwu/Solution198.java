b514522f7be5e78d590b850b17defe9a6580ef14
Hao_Wu_001443861 <wu.hao8@husky.neu.edu>
2019-03-24 13:22:05 -0400

    3.23

--- LCesay198/src/lcwu/Solution198.java
+++ LCesay198/src/lcwu/Solution198.java
@@ -7,8 +7,10 @@ class Solution198 {
         int Res[] = new int[nums.length+2];
         for(int i=0;i<Res.length-2;i++ ){
             Res[i+2]+=Math.max(nums[i]+Res[i],Res[i+1]);
+
         }
         return Res[Res.length-1];
+
     }
 }
 
