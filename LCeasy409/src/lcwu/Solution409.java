419c4c9711e4142bde0fdadf4b2e28266e788298
Hao_Wu_001443861 <wu.hao8@husky.neu.edu>
2019-04-03 17:18:58 -0400

    4.3

---
+++ LCeasy409/src/lcwu/Solution409.java
@@ -0,0 +1,16 @@
+package lcwu;
+
+public class Solution409 {
+    public int longestPalidrome(String s) {
+        int[] count = new int[128];
+        for (char c : s.toCharArray())
+            count[c]++;
+        int ans = 0;
+        for (int v : count) {
+            ans += ( v % 2 == 0 ) ? v : v-1 ;
+            if (ans % 2 == 0 && v % 2 == 1)
+                ans++;
+        }
+        return ans;
+    }
+}
